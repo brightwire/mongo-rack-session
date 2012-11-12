b3574fa6d0c6fec52c2cada7096b4f7f0d4e14a3
Marcus Brito <mbrito@gmail.com>
2012-11-12 15:59:22 -0200

    Greatly simplify the session implementation.
    The mongo storage is now completely hidden, and never presented to the
    rack application. Instead, a simple Hash is returned from get_session,
    and set_session takes that hash and stores it in the backing mongo
    document.

--- lib/mongo-rack-session.rb
+++ lib/mongo-rack-session.rb
@@ -1,16 +1,16 @@
 require 'rack/session/abstract/id'
 require 'mongo_mapper'
 require 'uuid'
-require 'active_support' # probably already included from mongo_mapper
 
 module Rack
   module Session
     class Mongo < Abstract::ID
-      VERSION = '0.1.1'
-      class Session
+      VERSION = '0.3.0'
+
+      class SessionDocument
         include MongoMapper::Document
 
-        key "Id", String
+        key :Id, String
         key :Expired, Boolean
         key :HostAddress, String
         key :LastAccessTime, Time
@@ -20,87 +20,47 @@ module Rack
         key :Attributes, Hash
 
         def is_valid?
-          if self.Timeout && self.StartTimestamp 
-            (self.StartTimestamp + (self.Timeout/1000).seconds > Time.now) ? self : nil
+          if self[:Timeout] && self[:LastAccessTime]
+            (self[:LastAccessTime] + (self[:Timeout] / 1000).seconds > Time.now) ? self : nil
           else
             nil
           end
         end
-
-        def key?(k)
-          self if self.class.key?(k) # || @Attributes.key?(k)
-        end
-
-        def sort_by(*args, &block)
-          flat_obj = send(:Attributes)
-          tmp_obj = {}
-          self.keys.each do |k,v|
-            unless k == "Attributes"
-              tmp_obj[k] = send(k.to_sym)
-            end
-          end
-          flat_obj.update tmp_obj
-
-          block.call(flat_obj)
-        end
-
-        def []=(k,val)
-          if k == 'flash'
-          elsif self.class.key?(k)
-            send("#{k}=".to_sym, val)
-          else
-            self.Attributes[k] = val
-          end
-        end
-        def [](k)
-          if k == 'flash'
-            @flash ||= ActionDispatch::Flash::FlashHash.new
-          elsif self.class.key?(k)
-            send(k.to_sym)
-          else
-            @Attributes[k]
-          end
-        end
-        
       end
 
-      DEFAULT_OPTIONS = Abstract::ID::DEFAULT_OPTIONS.merge \
+      DEFAULT_OPTIONS = Abstract::ID::DEFAULT_OPTIONS.merge({
         :mongo_db_name => 'shiro',
         :mongo_collection_name => 'Sessions',
         :mongo_server => 'mongohost',
-        :default_key => 'JSESSIONID',  # cookie key
-        :session_class_key => 'Id'
+        :default_key => 'JSESSIONID'})
 
       def initialize(app, options={})
         super
         @key = options[:key] || @default_options[:default_key]
-
-        # set up the Session Class either defined by the User or using the one defined above
-        @@session_class = options[:session_class] || Session
-        @@session_class.set_database_name(options[:mongo_db_name] || @default_options[:mongo_db_name])
-        @@session_class.set_collection_name(options[:mongo_collection_name] || @default_options[:mongo_collection_name])
-        @@session_class.connection(::Mongo::Connection.new(options[:mongo_server] || @default_options[:mongo_server]))
-        @@session_class_key = options[:session_class_key] || @default_options[:session_class_key]
+        SessionDocument.set_database_name(options[:mongo_db_name] || @default_options[:mongo_db_name])
+        SessionDocument.set_collection_name(options[:mongo_collection_name] || @default_options[:mongo_collection_name])
+        SessionDocument.connection(::Mongo::Connection.new(options[:mongo_server] || @default_options[:mongo_server]))
       end
 
       def get_session(env, sid)
-        sid ||= generate_sid
-        session = find_session(env, sid)
-        [sid, session]
+        document = find_session(env, sid)
+        [document[:Id], document[:Attributes]]
       end
 
-      def set_session(env, session_id, new_session, options)
-        session = find_session(env, session_id)
-        (session && new_session && session.Id == new_session.Id && new_session.save) ? new_session.Id : nil
+      def set_session(env, sid, session, options)
+        document = find_session(env, sid)
+        document[:Attributes] = session
+        document[:LastAccessTime] = Time.now
+        document[:Id] if document.save
       end
 
       def find_session(env, sid)
-        mongo_session = @@session_class.first(:conditions => {@@session_class_key => sid})
+        document = SessionDocument.first(:conditions => {:Id => sid}) if sid.present?
+        timestamp = Time.now
 
-        unless mongo_session.try(:is_valid?)
-          timestamp = Time.now
-          mongo_session = @@session_class.create(
-            :Id => sid,
+        unless document.try(:is_valid?)
+          document = SessionDocument.create(
+            :Id => generate_sid,
             :Expired => false,
             :HostAddress => env['action_dispatch.remote_ip'].to_s,
             :LastAccessTime => timestamp,
@@ -109,7 +69,7 @@ module Rack
             :Attributes => {"org.apache.shiro.subject.support.DefaultSubjectContext_AUTHENTICATED_SESSION_KEY" => false})
         end
 
-        mongo_session
+        document
       end
 
       def generate_sid
@@ -118,4 +78,3 @@ module Rack
     end
   end
 end
-
